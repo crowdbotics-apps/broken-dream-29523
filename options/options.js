ac10a926f30b1611ee96df146ad76b0a0ff54f73
Crowdbotics <team@crowdbotics.com>
2021-08-05 17:17:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "broken_dream_29523Identifier",
+  url: "https://broken_dream_29523Identifier.botics.co",
+  api: "https://broken_dream_29523Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
